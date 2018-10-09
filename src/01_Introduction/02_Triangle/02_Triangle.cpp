695a1ab06b23752ebe4f71102220d38889ff96f0
Ubpa <641614112@qq.com>
2018-10-09 23:22:36 +0800

    Update 01_Introduction/02_Triangle with exercises

--- src/01_Introduction/02_Triangle/02_Triangle.cpp
+++ src/01_Introduction/02_Triangle/02_Triangle.cpp
@@ -16,38 +16,41 @@ int main(int argc, char ** argv) {
 	char title[] = "02_Triangle";
 	Glfw::GetInstance()->Init(width, height, title);
 	//------------
-	float vertices[] = {
+	float vertices0[] = {
 		0.5f, 0.5f, 0.0f,   // 右上角
 		0.5f, -0.5f, 0.0f,  // 右下角
 		-0.5f, -0.5f, 0.0f, // 左下角
-		-0.5f, 0.5f, 0.0f   // 左上角
+		-0.5f, 0.5f, 0.0f,   // 左上角
+	};
+	float vertices1[] = {
+		-0.5f, -0.5f, 0.0f, // 左下角
+		-0.5f, 0.5f, 0.0f,   // 左上角
+		-1.0f, 0.0f, 0.0f   // 左
 	};
 	size_t indices[] = { // 注意索引从0开始! 
-	0, 1, 3, // 第一个三角形
-	1, 2, 3  // 第二个三角形
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
 	};
 	//------------ 0. 生成 VBO , VAO 和 EBO
-	size_t VBO;
-	glGenBuffers(1, &VBO);
-	size_t VAO;
-	glGenVertexArrays(1, &VAO);
-	size_t EBO[2];
-	glGenBuffers(2, EBO);
+	size_t VBO[2];
+	glGenBuffers(2, VBO);
+	size_t VAO[2];
+	glGenVertexArrays(2, VAO);
+	size_t EBO;
+	glGenBuffers(1, &EBO);
 	//------------ 1. 绑定 VAO
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO[0]);
 	//------------ 2. 绑定 VBO, 将顶点数据送到 VBO 中
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	/*
 	* GL_STATIC_DRAW ：数据不会或几乎不会改变。
 	* GL_DYNAMIC_DRAW：数据会被改变很多。
 	* GL_STREAM_DRAW ：数据每次绘制时都会改变。
 	*/
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices0), vertices0, GL_STATIC_DRAW);
 	//------------ 3. 绑定 EBO, 将索引数据送到 EBO 中
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(size_t), indices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(size_t), &(indices[0])+3, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	//------------ 4. 绑定顶点属性指针
 	/*
 	* @1 Location : 0
@@ -59,6 +62,12 @@ int main(int argc, char ** argv) {
 	* @5 Stride : 3 * sizeof(float)
 	* @6 Offset : 0
 	*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+	glEnableVertexAttribArray(0);
+	//------------ 5. 重复1, 2 和 4, 再设定一个 VAO
+	glBindVertexArray(VAO[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
 	glEnableVertexAttribArray(0);
 	//------------ 5. 编译着色器
@@ -78,22 +87,23 @@ int main(int argc, char ** argv) {
 	LambdaOperation renderOp([&]() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glBindVertexArray(VAO);
 		//------------ Red
+		glBindVertexArray(VAO[0]);
 		shaderRed.Use();
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
 		/*
 		* @1 绘制模式
 		* @2 绘制的顶点个数
 		* @3 索引类型
 		* @4 偏移量
 		*/
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 		//------------ Blue
+		glBindVertexArray(VAO[1]);
 		shaderBlue.Use();
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//------------ 交换缓冲
 		glfwSwapBuffers(Glfw::GetInstance()->GetWindow());
+		//------------ 拉取事件
 		glfwPollEvents();
 	}, true);
 
